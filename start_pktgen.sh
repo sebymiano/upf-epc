f892435fe5ed39a0ceb1a27c5d2b8f2382842783
Sebastiano Miano <mianosebastiano@gmail.com>
2022-02-09 14:03:58 +0000

    Fix start pktgen script

--- start_pktgen.sh
+++ start_pktgen.sh
@@ -7,5 +7,6 @@ sudo docker run --name pktgen -td --restart unless-stopped --cpuset-cpus=3,5,7,9
                 -v "$PWD/conf":/opt/bess/bessctl/conf --device=/dev/vfio/vfio --device=/dev/vfio/113 \
                 --device=/dev/vfio/114 upf-epc-bess:"$(<VERSION)" -grpc-url=0.0.0.0:10514
 
+sleep 20
 sudo docker exec -d -it pktgen ./bessctl http 0.0.0.0 8000
 sudo docker exec -it -d pktgen ./bessctl run pktgen
