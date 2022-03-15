6de4949384ddf522a8509203200d634a5e9339ed
Sebastiano Miano <mianosebastiano@gmail.com>
2022-03-15 18:08:57 +0000

    Update QoS on eBPF dataplane

--- ptf/tests/linerate/baseline.py
+++ ptf/tests/linerate/baseline.py
@@ -186,45 +186,44 @@ class UplinkPerformanceBaselineTest(TrexTest, GrpcTest):
         startIP = IPv4Address('16.0.0.1')
         endIP = startIP + UE_COUNT - 1
 
-        # program UPF for downlink traffic by installing PDRs and FARs
-        # print("Installing PDRs and FARs...")
-        # for i in range(UE_COUNT):
-        #     # install N6 DL PDR to match UE dst IP
-        #     pdrUp = self.createPDR(
-        #         srcIface = ACCESS,
-        #         srcIP = int(startIP + i),
-        #         tunnelIP4Dst=int(N3_IP),
-        #         tunnelTEID=1,
-        #         srcIfaceMask = 0xFF,
-        #         srcIPMask=0xFFFFFFFF,
-        #         tunnelIP4DstMask=0xFFFFFFFF,
-        #         tunnelTEIDMask=0xFFFF,
-        #         precedence = 255,
-        #         fseID = n3TEID + i + 1, # start from 1
-        #         ctrID = 0,
-        #         farID = i,
-        #         qerIDList = [1],
-        #         needDecap = 1,
-        #     )
-        #     self.addPDR(pdrUp)
+        # program UPF for uplink traffic by installing PDRs and FARs
+        print("Installing PDRs and FARs...")
+        for i in range(UE_COUNT):
+            pdrUp = self.createPDR(
+                srcIface = ACCESS,
+                srcIP = int(startIP + i),
+                tunnelIP4Dst=int(N3_IP),
+                tunnelTEID=1,
+                srcIfaceMask = 0xFF,
+                srcIPMask=0xFFFFFFFF,
+                tunnelIP4DstMask=0xFFFFFFFF,
+                tunnelTEIDMask=0xFFFF,
+                precedence = 255,
+                fseID = n3TEID + i + 1, # start from 1
+                ctrID = 0,
+                farID = i,
+                qerIDList = [1],
+                needDecap = 1,
+            )
+            self.addPDR(pdrUp)
 
-        #     farUp = self.createFAR(
-        #         farID = i,
-        #         fseID = n3TEID + i + 1, # start from 1
-        #         applyAction = ACTION_FORWARD,
-        #         dstIntf = DST_CORE,
-        #     )
-        #     self.addFAR(farUp)
+            farUp = self.createFAR(
+                farID = i,
+                fseID = n3TEID + i + 1, # start from 1
+                applyAction = ACTION_FORWARD,
+                dstIntf = DST_CORE,
+            )
+            self.addFAR(farUp)
 
-        #     qer = self.createQER(
-        #         gate=GATE_METER,
-        #         qerID=1,
-        #         fseID=n3TEID + i + 1,
-        #         ulMbr=mbr_kbps,
-        #         dlMbr=mbr_kbps,
-        #         burstDurationMs=burst_ms,
-        #     )
-        #     self.addApplicationQER(qer)
+            qer = self.createQER(
+                gate=GATE_METER,
+                qerID=1,
+                fseID=n3TEID + i + 1,
+                ulMbr=mbr_kbps,
+                dlMbr=mbr_kbps,
+                burstDurationMs=burst_ms,
+            )
+            self.addApplicationQER(qer)
 
         # set up trex to send traffic thru UPF
         print("Setting up TRex client...")
